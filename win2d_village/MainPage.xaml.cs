7c4fe93718edca84491ec1927eeca150c350ef73
joshbax <joshbax@JOSHBAX-DESKTOP>
2016-06-24 16:56:02 -0700

    Add project files.

---
+++ win2d_village/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Microsoft.Graphics.Canvas.UI;
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Windows.UI;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace win2d_village
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        CanvasBitmap characterSource;
+        ChromaKeyEffect characterMap;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void gridMain_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+
+        }
+
+        private void gridMain_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+
+        }
+
+        private void canvasMain_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        {
+            args.DrawingSession.DrawImage(characterMap, 0, 0);
+        }
+
+        private void canvasMain_Update(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
+        {
+
+        }
+
+        private void canvasMain_CreateResources(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs args)
+        {
+            args.TrackAsyncAction(CreateResourcesAsync(sender).AsAsyncAction());
+        }
+
+        async Task CreateResourcesAsync(CanvasAnimatedControl sender)
+        {
+            characterSource = await CanvasBitmap.LoadAsync(sender, "img\\characters.png", 96, CanvasAlphaMode.Ignore);
+
+            characterMap = new ChromaKeyEffect();
+            characterMap.Color = Color.FromArgb(255, 255, 0, 255);
+            characterMap.Source = characterSource;
+        }
+    }
+}
